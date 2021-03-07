50ecfc804cc2983e34d193a05970482bf44d6702
tagucch <y.t.3k.gunners@gmail.com>
2021-03-07 20:36:13 +0900

    dateのフォーマットを変更

--- components/date.tsx
+++ components/date.tsx
@@ -1,7 +1,11 @@
 import { parseISO, format } from 'date-fns'
 
 const Date = ({ dateString }) => {
-  const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
+  const date: Date = parseISO(dateString)
+  return (
+    <time dateTime={dateString}>
+      {format(date, 'yyyy-MM-dd HH:mm:ss')}
+    </time>
+  )
 }
 export default Date
