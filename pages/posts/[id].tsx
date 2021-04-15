060ab814603654be53ffaded992635afe1cf3384
tagucch <y.t.3k.gunners@gmail.com>
2021-04-16 01:59:50 +0900

    記事ページのスタイル調整とか型定義追加とか

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,28 +1,66 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import {
+  GetStaticProps,
+  GetStaticPropsContext,
+  GetStaticPaths
+} from 'next'
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import Date from '../../components/date'
 
-const Post = ({ postData }) => {
+const Post = ({
+  postData
+}: {
+  postData: {
+    id: string,
+    contentHtml: string,
+    title: string,
+    date: string,
+    tags: string[]
+  }
+}) => {
   return (
     <Layout>
-      <Head>
-        {postData.title}
-      </Head>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      <Date dateString={postData.date} />
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <section className="flex flex-col m-0 w-3/5 mx-auto min-w-min mt-5 break-words">
+        <Head>
+          {postData.title}
+        </Head>
+        <div className="font-extrabold text-3xl splatfont">
+          {postData.title}
+        </div>
+        <div className="mt-2 flex">
+          {postData.tags.map(tag => {
+            return (
+            <div key={tag} className="bg-green-300 px-1 mr-2">
+              <Link href={`/tags/${tag}`}>
+                {tag}
+              </Link>
+            </div>)
+          })}
+        </div>
+        <div className="mt-2">
+          <Date dateString={postData.date} />
+        </div>
+        <div className="mt-3">
+          <article
+            className="content"
+            dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
+          />
+        </div>
+        <div className="mt-2">
+          <Link href="/">
+            <a className="splatfont text-2xl">← Back To Home</a>
+          </Link>
+        </div>
+      </section>
     </Layout>
   )
 }
 
 export default Post
 
-export const getStaticPaths = async () => {
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds()
   return {
     paths,
@@ -30,8 +68,8 @@ export const getStaticPaths = async () => {
   }
 }
 
-export const getStaticProps = async ({ params }) => {
-  const postData = await getPostData(params.id)
+export const getStaticProps: GetStaticProps = async ({ params }: GetStaticPropsContext) => {
+  const postData = await getPostData(params?.id as string)
   return {
     props: {
       postData
