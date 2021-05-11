3499a4cac7f649c80cf0c2775f076c30cd1942df
tagucch <y.t.3k.gunners@gmail.com>
2021-05-12 01:53:02 +0900

    rootのページをの見た目を大きく変えた & 型追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Layout from '../components/layout'
+import { GetStaticProps } from 'next'
 import { getSortedPostData } from '../lib/posts'
 import Date from '../components/date'
 
@@ -10,20 +11,32 @@ const Home = ({
     id: string
     date: string
     title: string
+    tags: string[]
   }[]
 }) => {
   return (
     <Layout home>
-      <section className="text-xl pt-px mt-5">
-        <ul className="list-none m-0 w-1/2 mx-auto min-w-max">
-          {allPosts.map(({ id, date, title }) => (
-            <li className="mr-4 mb-4 last:mb-0" key={id}>
+      <section className="text-xl pt-px mx-auto w-4/5 md:w-1/2 max-w-full">
+        <ul className="list-none m-0 mx-auto">
+          {allPosts.map(({ id, date, title, tags }) => (
+            <li className="mb-10 last:mb-0" key={id}>
               <div className="text-base tracking-wider">
                 <Date dateString={date}></Date>
               </div>
               <Link href={`/posts/${id}`}>
-                <a className="text-2xl splatfont">{title}</a>
+                <a className="text-2xl splatfont">
+                  {title.length > 35 ? title.slice(0, 35).concat('…') : title}
+                </a>
               </Link>
+              <div className="flex mt-2">
+                {tags.map(tag => (
+                  <div className="bg-green-300 text-base px-1.5 mr-2">
+                    <Link href={`/tags/${tag}`}>
+                      {tag}
+                    </Link>
+                  </div>
+                ))}
+              </div>
             </li>
           ))}
         </ul>
@@ -34,7 +47,7 @@ const Home = ({
 
 export default Home
 
-export const getStaticProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const allPosts = getSortedPostData()
   return {
     props: {
