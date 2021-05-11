6bc2346e904278cb2bfd90da7c507f93140759de
tagucch <y.t.3k.gunners@gmail.com>
2021-05-12 01:52:42 +0900

    Layoutを大きく変えた

--- components/layout.tsx
+++ components/layout.tsx
@@ -10,7 +10,8 @@ const Layout = ({
     home?: boolean
   }) => {
   return (
-    <div className="mx-auto h-full min-h-screen bg-indigo-100 min-w-min">
+    <div className="min-h-screen min-w-min max-w-full bg-gray-100">
+    {/* // <div className="min-h-screen min-w-min text-sm md:text-base"> */}
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta
@@ -26,19 +27,19 @@ const Layout = ({
         <meta name="og:title" content={title} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
-      <header className="flex flex-col items-center bg-blue-200 pt-2 pb-5 splatfont">
+      <header className="flex flex-col items-center pt-6 mb-10 splatfont mx-auto w-4/5 md:w-1/2">
         {home ? (
-          <h1 className="text-4xl font-extrabold my-4">{title}</h1>
+          <div className="md:text-4xl sm:text-3xl text-xl font-extrabold my-4 text-gray-900">{title}</div>
         ) : (
-          <h1 className="text-4xl font-extrabold my-4">
+          <div className="md:text-4xl sm:text-3xl text-xl font-extrabold my-4">
             <Link href="/">
-              <a className="text-current hover:no-underline">{title}</a>
+              <a className="text-current hover:no-underline text-gray-900">{title}</a>
             </Link>
-          </h1>
+          </div>
         )}
-        <h2 className="splatfont">{desc}</h2>
+        <h2 className="text-gray-700 text-base break-words max-w-xs md:max-w-md text-center">{desc}</h2>
       </header>
-      <main className="pb-10">{children}</main>
+      <main className="pb-10 mx-auto">{children}</main>
     </div>
   )
 }
