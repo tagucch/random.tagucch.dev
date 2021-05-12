5ad7001c34fcf6c8628e9085697ccccaeef16cac
tagucch <y.t.3k.gunners@gmail.com>
2021-05-12 21:47:53 +0900

    色に熨斗目花色と枡花色を追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    backgroundColor: theme => ({
+      ...theme('colors'),
+      'noshimehana': '#426579',
+      'masuhana': '#5b7e91'
+    }),
+    textColor: theme => ({
+      ...theme('colors'),
+      'noshimehana': '#426579',
+      'masuhana': '#5b7e91'
+    }),
     extend: {
     },
   },
