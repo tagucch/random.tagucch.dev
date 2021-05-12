839ecf063d5ec8fb3498042f3c15f088b75a3313
tagucch <y.t.3k.gunners@gmail.com>
2021-05-12 21:49:32 +0900

    スタイル調整

--- pages/tags/index.tsx
+++ pages/tags/index.tsx
@@ -9,7 +9,7 @@ const Tags = ({ allTagsAndCounts }: { allTagsAndCounts: TagsAndCounts }) => {
       <section className="mx-auto w-4/5 md:w-1/2">
         <ul className="list-none m-0 mx-auto flex flex-wrap">
           {allTagsAndCounts.map(({ tagName, count }) => (
-            <li className="mr-6" key={tagName}>{tagName} ({count})</li>
+            <a><li className="py-0.5 px-1.5 mb-4 mr-4 bg-noshimehana text-white" key={tagName}>{tagName} ({count})</li></a>
           ))}
         </ul>
       </section>
