80fb063c38e0121339fcd1efaf6ff308d6797f58
tagucch <y.t.3k.gunners@gmail.com>
2021-05-14 21:13:41 +0900

    aタグの位置を修正

--- pages/tags/index.tsx
+++ pages/tags/index.tsx
@@ -9,7 +9,9 @@ const Tags = ({ allTagsAndCounts }: { allTagsAndCounts: TagsAndCounts }) => {
       <section className="mx-auto w-4/5 md:w-1/2">
         <ul className="list-none m-0 mx-auto flex flex-wrap">
           {allTagsAndCounts.map(({ tagName, count }) => (
-            <a><li className="py-0.5 px-1.5 mb-4 mr-4 bg-noshimehana text-white" key={tagName}>{tagName} ({count})</li></a>
+              <li className="py-0.5 px-1.5 mb-4 mr-4 bg-noshimehana text-white" key={tagName}>
+                <Link href={`/tags/${encodeURIComponent(tagName)}`}><a>{tagName} ({count})</a></Link>
+              </li>
           ))}
         </ul>
       </section>
