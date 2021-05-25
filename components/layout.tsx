291a66fc4fcee2edccfa672a58f07240d9c67707
tagucch <y.t.3k.gunners@gmail.com>
2021-05-26 00:56:35 +0900

    /publicいらなかったかも

--- components/layout.tsx
+++ components/layout.tsx
@@ -31,7 +31,7 @@ const Layout = ({
           // content={`https://og-image.now.sh/${encodeURI(
           //   title
           // )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-          content={`${baseUrl}/public/images/ogp.jpg`}
+          content={`${baseUrl}/images/ogp.jpg`}
         />
         <meta name="og:site_name" key="og:site_name" content={title} />
         <meta name="og:title" key="og:title" content={title} />
