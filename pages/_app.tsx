bea99e24246d1666611ab3a248ab1b4d4eef3b3c
tagucch <y.t.3k.gunners@gmail.com>
2021-09-10 01:33:46 +0900

    solarizedlight.cssのスタイルを読み込む

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app'
 import '../styles/global.css'
+import 'prismjs/themes/prism-solarizedlight.css'
 import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }: AppProps) {
