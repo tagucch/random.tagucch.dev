fb78e857809502511379e8f8a4416feb9af811ca
tagucch <y.t.3k.gunners@gmail.com>
2021-09-10 01:34:03 +0900

    dynamic importでダークモードとライトモードのCSSのimportの条件分岐をしようとしたけど、結局できなかったので痕跡だけ残す

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,13 @@ import '../styles/global.css'
 import 'prismjs/themes/prism-solarizedlight.css'
 import { ThemeProvider } from 'next-themes'
 
+// (async () => {
+//   if (theme === 'light') {
+//     await import (/* webpackChunkName: "prismjs/themes/prism-tomorrow.css" */ 'prismjs/themes/prism-tomorrow.css')
+//   }
+
+// })()
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
